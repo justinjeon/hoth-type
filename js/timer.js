79ecb699c7864cf2c5cf6eaa2c00097a68fdb3e3
Justin Jeon <justinjeon151@gmail.com>
2018-11-17 16:17:54 -0800

    Fixed timer

--- js/timer.js
+++ js/timer.js
@@ -1,24 +1,32 @@
 var time = document.getElementById('timer'),
-    start = document.getElementById('start'),
-    stop = document.getElementById('stop'),
-    clear = document.getElementById('clear'),
-    centiseconds = 0, seconds = 0, minutes = 0;
+    seconds = document.getElementById('seconds');
+    wpm = document.getElementById("wpm");
+    wpm.innerHTML = "0 wpm";
+    seconds = 0, minutes = 0;
 
 function add() {
         seconds++;
         if (seconds%60 == 0) {
             minutes++;
         }
-    
+    getWpm();
     time.textContent = (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "0") + ":" + (seconds%60 ? (seconds%60 > 9 ? seconds%60 : "0" + seconds%60) : "00");
-
     timer();
 }
 function timer() {
     t = setTimeout(add, 1000);
 }
 
-timer();
+function getWpm() {
+    wpm.innerHTML = ((correct/seconds)*12).toFixed(0) + " wpm";
+}
+
+function timeOn() {
+    if (setTimer == 0)
+        timer();
+    setTimer = 1;
+ }
+
 
 // /* Start button */
 // start.onclick = timer;
